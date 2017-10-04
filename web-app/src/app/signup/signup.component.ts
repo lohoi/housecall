f02617671e6b650aa6317ec399f9b69192fbb937
Albert Lo <lohoi@umich.edu>
2017-10-04 15:24:55 -0400

    data bind works

--- web-app/src/app/signup/signup.component.ts
+++ web-app/src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { User } from '../user';
 
 @Component({
   selector: 'app-signup',
@@ -7,26 +8,20 @@ import { Http } from '@angular/http';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  public user: User;
 
   constructor(private http: Http) {
-    this.http = http
+    this.http = http;
+    this.user = new User();
   }
 
   ngOnInit() {
+     // let this.User = new User();
   }
 
-  submitUser = function() {
-    // let params = {
-    //   email: ,
-    //   skype: ,
-    //   firstname: ,
-    //   lastname: ,
-    //   password: ,
-    //   specialty: ,
-    //   hospital_id: ,
-    //   user_type: ,
-    // }
-    console.log(this.user);
-    // this.http.post('http://localhost:3000/users', params).subscribe();
+  onSubmit = function() {
+    
+    // console.log(this.user);
+    this.http.post('http://localhost:3000/users', this.user).subscribe();
   }
 }
