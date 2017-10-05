ff9754149c5d1e47d8138cd186b1cabf06825633
Albert Lo <lohoi@umich.edu>
2017-10-04 21:46:13 -0400

    added slack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    puts params
-    @user.save
+    def create
+      @user = User.new(user_params)
+      respond_to do |format|
+        if @user.save
+          format.html {redirect_to @user, notice: 'User successfully created.'}
+          format.json {render :show, status: :created, location: @user}
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   def update
