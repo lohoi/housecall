4d2183ba163a7436b1a0c56c7643d2fd884cd657
emkess <emkess@umich.edu>
2017-10-06 14:15:56 -0400

    Display list of notes, added icons

--- web-app/src/app/notes/notes.component.ts
+++ web-app/src/app/notes/notes.component.ts
@@ -8,17 +8,29 @@ import { Http } from '@angular/http';
 })
 export class NotesComponent implements OnInit {
   noteText: string = "";
+  notes: [{
+    text: string,
+  }];
 
   constructor(private http: Http) { 
     this.http = http;
+    this.notes = [
+      {text: "one"},
+      {text: "two"}
+    ];
+    //http.get('http://localhost:3000/about.json').subscribe(res => this.text = res.json().message);
   }
 
   ngOnInit() {
   }
 
   saveNote = function(){
-    console.log( this.noteText);
+    console.log(this.noteText);
     //this.http.post('http://localhost:3000/addNote', this.noteText).subscribe();
+  }
+
+  deleteNote = function(){
+    console.log("delete");
   }
 
 }
