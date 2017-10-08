c7e0f97dfbefd16af29aa7c2bbca75850c51b16d
emkess <emkess@umich.edu>
2017-10-08 13:26:34 -0400

    Notes styling, add new note with name

--- web-app/src/app/notes/notes.component.ts
+++ web-app/src/app/notes/notes.component.ts
@@ -10,13 +10,18 @@ export class NotesComponent implements OnInit {
   noteText: string = "";
   notes: [{
     text: string,
+    name: string
   }];
 
   constructor(private http: Http) { 
     this.http = http;
     this.notes = [
-      {text: "one"},
-      {text: "two"}
+      {text: "one", name:"one"},
+      {text: "two", name:"one"},
+      {text: "three", name:"one"},
+      {text: "four", name:"one"},
+      {text: "five", name:"one"},
+      {text: "six", name:"one"},
     ];
     //http.get('http://localhost:3000/about.json').subscribe(res => this.text = res.json().message);
   }
@@ -31,6 +36,7 @@ export class NotesComponent implements OnInit {
 
   deleteNote = function(){
     console.log("delete");
+    //this.http.post('http://localhost:3000/deleteNote', this.noteText).subscribe();
   }
 
 }
