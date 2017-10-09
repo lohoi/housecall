da36686e9e0970c39f9d9e4c2f94ded647f92c1d
Albert Lo <lohoi@umich.edu>
2017-10-08 21:47:51 -0400

    added flags so devise doesnt act up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:skype, :firstname, :lastname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:skype, :firstname, :lastname, :hospital, :specialty])
  	end
 end
