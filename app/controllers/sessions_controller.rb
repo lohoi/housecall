9896fcaa93a76f07fdf588d6eb33c08eab23777c
Albert Lo <lohoi@umich.edu>
2017-10-10 18:32:24 -0400

    rem suggestions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by(email: params[:session][:email].downcase)
-    # respond_to do |format|
-    #   if user && user.authenticate(params[:session][:password])
-    #     log_in user
-    #     format.html { redirect_to user }
-    #   else
-    #     format.html { render :new, notice: "Login failed, invalid email/password combination" }
-    #   end
-    # end
   end
 
   def destroy
-    # log_out if logged_in?
-    # redirect_to root_url
   end
 
 end
