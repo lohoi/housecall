4d67cbaa6e1a61e87a789c2cbd49bf3643d55d2f
emkess <emkess@umich.edu>
2017-10-11 22:03:19 -0400

    Edit works! Toggle between check and pencil

--- web-app/src/app/notes/notes.component.ts
+++ web-app/src/app/notes/notes.component.ts
@@ -28,8 +28,14 @@ export class NotesComponent implements OnInit {
       search: new URLSearchParams('user_id=' + this.user_id)
     });
     console.log("get notes");
-    this.http.get('http://localhost:3000/notes.json', options).subscribe((res: Response) => {this.notes = res.json()});
+    this.http.get('http://localhost:3000/notes.json', options).subscribe((res: Response) => {this.notes = res.json(); this.setEdit()});
     console.log(this.notes);
+  }
+
+  setEdit(){
+    this.notes.forEach(note => {
+      note.edit = false;
+    });
   }
 
   saveNote = function(noteTitle: string, noteText: string){
@@ -53,7 +59,33 @@ export class NotesComponent implements OnInit {
   }
 
   editNote = function(id: number){
+    console.log(id);
     console.log("edit");
+    let note = new Note();
+    note.title = document.getElementById('title-'+id).innerHTML;
+    note.text = document.getElementById('text-'+id).innerHTML;
+    note.user_id = this.user_id;
+    note.id = id;
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    this.http.patch('http://localhost:3000/notes/' + id + '.json', JSON.stringify(note), { headers: headers }).subscribe((ok) => console.log(ok));      
+  }
+
+  showIcon = function(icon:string, id:number) {
+    var pencilIcon = document.getElementById('pencil-'+id);
+    var checkIcon = document.getElementById('check-'+id);
+    var tableElement = document.getElementById('tableElement-'+id);
+    if(icon === "pencil"){
+      tableElement.style.border = "1.8px solid #dcad51";
+      tableElement.style.fontFamily = 'Courier New';
+      pencilIcon.style.display = "none";
+      checkIcon.style.display = "block";
+    } else {
+      tableElement.style.border = ".6px solid #000";
+      tableElement.style.fontFamily = 'Roboto';
+      checkIcon.style.display = "none";
+      pencilIcon.style.display = "block";
+    }
   }
 
 }
