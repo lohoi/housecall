fb3daf800424961f2480ebc10d6eff4cc1683345
befost <befost@umich.edu>
2017-10-15 17:37:23 -0600

    mail login creds to new patient when doctor adds new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,8 @@
 class ContactsController < ApplicationController
   def create
     doctor = User.find(params[:doctor_id])
-    puts params
+
     password = Devise.friendly_token.first(8)
-    puts password
 
     patient = User.new(firstname: params[:firstname], lastname: params[:lastname], email: params[:email],
                       skype: params[:skype], password: password, user_type: 1)
@@ -13,6 +12,8 @@ class ContactsController < ApplicationController
 
     contact = Contact.new(doctor_id: doctor.id, patient_id: patient.id)
     contact.save
+
+    PatientMailer.account_activation(patient, password).deliver_now
 
     # respond_to do |format|
     #   if patient.save
