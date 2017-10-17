5a142a14a31d2dde5a1459aa064017955af4eb6c
Albert Lo <lohoi@umich.edu>
2017-10-17 14:07:54 -0400

    two way ddata bind so it doesn't reload the page

--- web-app/src/app/notes/notes.component.ts
+++ web-app/src/app/notes/notes.component.ts
@@ -3,6 +3,8 @@ import { Angular2TokenService } from 'angular2-token'
 import { Http, URLSearchParams, RequestOptions, Response, Headers } from '@angular/http';
 import { Note } from "../note"
 import { UserService } from "../user.service";
+import { User } from "../user";
+
 
 @Component({
   selector: 'app-notes',
@@ -11,12 +13,12 @@ import { UserService } from "../user.service";
 })
 export class NotesComponent implements OnInit {
   notes: [Note];
-  user_id;
+  user: User;
 
   constructor(public userService:UserService, private http: Http, private authService: Angular2TokenService) { 
     this.http = http;
     this.userService.getUser().subscribe((res) => {
-      this.user_id = this.authService.currentUserData.id;
+      this.user = this.authService.currentUserData;
       this.getData();
     }); 
   }
@@ -27,40 +29,50 @@ export class NotesComponent implements OnInit {
   getData(){
     let options = new RequestOptions({
       // Have to make a URLSearchParams with a query string
-      search: new URLSearchParams('user_id=' + this.user_id)
+      search: new URLSearchParams('user_id=' + this.user.id)
     });
     console.log("get notes");
-    console.log(this.user_id)
-    this.http.get('http://localhost:3000/notes.json', options).subscribe((res: Response) => {this.notes = res.json(); this.setEdit()});
-    console.log(this.notes);
+    console.log(this.user.id)
+    this.http.get('http://localhost:3000/notes.json', options).subscribe(
+      (res: Response) => {
+          this.notes = res.json();
+          this.setEdit();
+          console.log("notes: ", this.notes);
+        }
+    );
   }
 
   setEdit(){
     this.notes.forEach(note => {
       note.edit = false;
     });
+
   }
 
   saveNote = function(noteTitle: string, noteText: string){
+    console.log("save note called!")
     let note = new Note();
     note.title = this.newNoteTitle;
     note.text = this.newNoteText;
-    note.user_id = this.user_id;
+    note.user_id = this.user.id;
     let dic = {note: note};
     console.log(dic);
+
+    this.notes.push(note);
     
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     this.http.post('http://localhost:3000/notes.json', JSON.stringify(dic), { headers: headers }).subscribe((ok) => console.log(ok));
     this.newNoteText = "";
     this.newNoteTitle = "";
-    location.reload();
   }
 
   deleteNote = function(id: number){
     console.log("delete note");
+
+    let delete_idx = this.notes.findIndex(note => note.id == id);
+    this.notes.splice(delete_idx,1);
     this.http.delete('http://localhost:3000/notes/' + id + '.json').subscribe((res: Response) => console.log(res.json));
-    location.reload();
   }
 
   editNote = function(id: number){
@@ -69,7 +81,7 @@ export class NotesComponent implements OnInit {
     let note = new Note();
     note.title = document.getElementById('title-'+id).innerHTML;
     note.text = document.getElementById('text-'+id).innerHTML;
-    note.user_id = this.user_id;
+    note.user_id = this.user.id;
     note.id = id;
     let dic = {note: note}
     let headers = new Headers();
