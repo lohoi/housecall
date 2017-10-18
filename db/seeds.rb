4a7aab74951ad0ed790d5ac1ad23e09229229994
Albert Lo <lohoi@umich.edu>
2017-10-17 22:04:21 -0400

    tooltips on navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'patient@test.com', firstname: 'Spaceboy', lastname: 'Tabman', password: "password", skype: "fakeskypeacc", user_type: 1)
-User.create!(email: 'doctor@test.com', firstname: 'Greatest', lastname: 'Ever', password: "password", skype: "skypeacc", user_type: 0)
+User.create!(email: 'patient@test.com', firstname: 'Spaceboy', lastname: 'Tabman', password: "password", skype: "fakeskypeacc", user_type: 1, hospital_id: 0)
+User.create!(email: 'doctor@test.com', firstname: 'Greatest', lastname: 'Ever', password: "password", skype: "skypeacc", user_type: 0, hospital_id: 0)
