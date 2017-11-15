01753aeba4974987eef9112c98c5f363fad7ae1e
Albert Lo <lohoi@umich.edu>
2017-11-15 16:13:28 -0500

    syntax err

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -59,7 +59,7 @@ const routes: Routes = [
     canActivate: [ AuthGuard ]
   },
   {
-    path: '/auth/password/edit',
+    path: 'auth/password/edit',
     component: ChangePasswordComponent,
     canActivate: [ AuthGuard ]
   },
