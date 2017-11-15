0738bb94edb9db8e18a5f9b5c28bc85784e9f911
Albert Lo <lohoi@umich.edu>
2017-11-15 15:42:16 -0500

    trying this out

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://housecall.herokuapp.com/api/',
   token_auth_config: {
-    apiBase: 'https://housecall.herokuapp.com/',
+    apiBase: 'https://housecall.herokuapp.com',
     // signInPath: 'auth/sign_in',
     // resetPasswordCallback: 'https://housecall.herokuapp.com/login',
     userTypes: [
