b1d3bb99ad99afd3ddbe7ffef629ad549568ed0b
Albert Lo <lohoi@umich.edu>
2017-11-14 21:18:07 -0500

    https

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://housecall.herokuapp.com/',
+  apiUrl: 'https://housecall.herokuapp.com/',
   token_auth_config: {
-    apiBase: 'http://housecall.herokuapp.com/',
+    apiBase: 'https://housecall.herokuapp.com/',
     signInPath: 'auth/sign_in',
     userTypes: [
         { name: 'doctor', path: '' },
