911131bc755a3b39e1a57532f1113e26304d798f
Albert Lo <lohoi@umich.edu>
2017-11-15 15:54:31 -0500

    testing

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true,
   apiUrl: 'https://housecall.herokuapp.com/api/',
   token_auth_config: {
-    apiBase: 'https://housecall.herokuapp.com//',
-    // signInPath: 'auth/sign_in',
+    apiBase: 'https://housecall.herokuapp.com',
+    signInPath: 'auth/sign_in',
+    resetPasswordPath: 'auth/password',
     // resetPasswordCallback: 'https://housecall.herokuapp.com/login',
     userTypes: [
         { name: 'doctor', path: '' },
