1f3a35d52e3503bae4985b0a5b80709abed7f9c4
Albert Lo <lohoi@umich.edu>
2017-12-09 17:15:43 -0500

    add validation to login page as well

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -36,6 +36,11 @@ export class LoginComponent implements OnInit {
     audio.play();
   }
 
+  validateEmail = function() {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(this.user.email);
+  }
+
 
   onSubmit = function() {
     event.preventDefault();
